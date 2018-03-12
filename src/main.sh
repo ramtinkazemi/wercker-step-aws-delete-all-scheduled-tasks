b5bc00f3a90b6383cf5c181a49817ae75ea045b5
Ramtin Kazemi <ramtin.kazemi@cashrewards.com>
2018-03-12 17:45:21 +1100

    v 0.0.7

--- src/main.sh
+++ src/main.sh
@@ -19,11 +19,11 @@ if [ -n "${STEP_AWS_DEFAULT_REGION}" ]; then
   aws configure set region ${STEP_AWS_DEFAULT_REGION} --profile ${STEP_AWS_PROFILE}
 fi
 
-warn "Collection Input Data for Target template"
-TASK_DEFINITION_ARN=$(aws ecs describe-task-definition --profile ${STEP_AWS_PROFILE} --task-definition ${STEP_TASK_DEFINITION} --query taskDefinition.taskDefinitionArn --output text)
-_AWS_ACCOUNT=$(aws sts get-caller-identity --profile ${STEP_AWS_PROFILE} --output text --query 'Account')
-ROLE_ARN=arn:aws:iam::${_AWS_ACCOUNT}:role/ecsEventsRole
-CLUSTER_ARN=$(aws ecs describe-clusters --profile ${STEP_AWS_PROFILE} --clusters ${STEP_CLUSTER} --query clusters[0].clusterArn --output text)
+#warn "Collection Input Data for Target template"
+#TASK_DEFINITION_ARN=$(aws ecs describe-task-definition --profile ${STEP_AWS_PROFILE} --task-definition ${STEP_TASK_DEFINITION} --query taskDefinition.taskDefinitionArn --output text)
+#_AWS_ACCOUNT=$(aws sts get-caller-identity --profile ${STEP_AWS_PROFILE} --output text --query 'Account')
+#ROLE_ARN=arn:aws:iam::${_AWS_ACCOUNT}:role/ecsEventsRole
+#CLUSTER_ARN=$(aws ecs describe-clusters --profile ${STEP_AWS_PROFILE} --clusters ${STEP_CLUSTER} --query clusters[0].clusterArn --output text)
 
 
 # warn "Remove target : ${STEP_TARGET_ID}"
