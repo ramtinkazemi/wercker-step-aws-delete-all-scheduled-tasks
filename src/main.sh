cd98c8603a836769657585fd8925eb220c3655d9
Ramtin Kazemi <ramtin.kazemi@cashrewards.com>
2018-03-12 17:37:59 +1100

    v 0.0.5

--- src/main.sh
+++ src/main.sh
@@ -17,14 +17,14 @@ aws configure set aws_access_key_id ${STEP_AWS_ACCESS_KEY_ID} --profile ${STEP_A
 aws configure set aws_secret_access_key ${STEP_AWS_SECRET_ACCESS_KEY} --profile ${STEP_AWS_PROFILE}
 if [ -n "${STEP_AWS_DEFAULT_REGION}" ]; then
   aws configure set region ${STEP_AWS_DEFAULT_REGION} --profile ${STEP_AWS_PROFILE}
-  warn "Here 1"
 fi
 
 warn "Collection Input Data for Target template"
-#TASK_DEFINITION_ARN=$(aws ecs describe-task-definition --profile ${STEP_AWS_PROFILE} --task-definition ${STEP_TASK_DEFINITION} --query taskDefinition.taskDefinitionArn --output text)
+TASK_DEFINITION_ARN=$(aws ecs describe-task-definition --profile ${STEP_AWS_PROFILE} --task-definition ${STEP_TASK_DEFINITION} --query taskDefinition.taskDefinitionArn --output text)
 _AWS_ACCOUNT=$(aws sts get-caller-identity --profile ${STEP_AWS_PROFILE} --output text --query 'Account')
 ROLE_ARN=arn:aws:iam::${_AWS_ACCOUNT}:role/ecsEventsRole
-#CLUSTER_ARN=$(aws ecs describe-clusters --profile ${STEP_AWS_PROFILE} --clusters ${STEP_CLUSTER} --query clusters[0].clusterArn --output text)
+CLUSTER_ARN=$(aws ecs describe-clusters --profile ${STEP_AWS_PROFILE} --clusters ${STEP_CLUSTER} --query clusters[0].clusterArn --output text)
+
 
 # warn "Remove target : ${STEP_TARGET_ID}"
 # aws events remove-targets \
@@ -38,13 +38,13 @@ ROLE_ARN=arn:aws:iam::${_AWS_ACCOUNT}:role/ecsEventsRole
 #     --profile ${STEP_AWS_PROFILE} \
 #     --name "${STEP_SCHEDULE_RULE_NAME}" 
 
-rules=$(aws events list-rules --name-prefix $STEP_APP_NAME --output text --query "Rules[*].Name")
+rules=$(aws --profile $STEP_AWS_PROFILE events list-rules --name-prefix $STEP_APP_NAME --output text --query "Rules[*].Name")
 for rule in $rules; do 
-    targets=$(aws events list-targets-by-rule --rule $rule --output text --query "Targets[*].Id")
+    targets=$(aws --profile $STEP_AWS_PROFILE events list-targets-by-rule --rule $rule --output text --query "Targets[*].Id")
     for target in $targets; do 
         warn "Removing target: $target"     
-        aws events remove-targets --rule $rule --ids $target;
+        aws --profile $STEP_AWS_PROFILE events remove-targets --rule $rule --ids $target;
     done
     warn "Deleting rule: $rule"
-    aws dev events delete-rule --name $rule
+    aws --profile $STEP_AWS_PROFILE events delete-rule --name $rule
 done
